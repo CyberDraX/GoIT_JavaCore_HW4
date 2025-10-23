b10009352fd4def55606f07d1e499508782f82f9
Vadim Korolov <vadim.v.korolov@gmail.com>
2025-10-24 00:33:05 +0300

    Task completed 2

--- src/main/java/UberShop.java
+++ src/main/java/UberShop.java
@@ -1,7 +1,20 @@
+import java.util.Arrays;
+
 public class UberShop {
     public void printPrices(float[] prices) {
         for (int i = 0; i < prices.length; i++) {
             System.out.println(prices[i] + " jup.");
+        }
+    }
+
+    public void multiplyPrices(float[] prices) {
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < 1000) {
+                prices[i] *= 2;
+            } else {
+                prices[i] *= 1.5f;
+            }
         }
     }
 
@@ -11,5 +24,11 @@ public class UberShop {
         //Should be 100 jup. 23.5 jup. 400 jup. - one price per line
         float[] prices = new float[] {100f, 23.5f, 400f};
         shop.printPrices(prices);
+
+        //Should be [200, 2250]
+        float[] prices1 = new float[] {100f, 1500f};
+        shop.multiplyPrices(prices1);
+        System.out.println(Arrays.toString(prices1));
+
     }
 }
