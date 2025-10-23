9a25a1649c265ae221ff46b3623baa2849250c94
Vadim Korolov <vadim.v.korolov@gmail.com>
2025-10-23 23:57:57 +0300

    Task completed 1

---
+++ src/main/java/UberShop.java
@@ -0,0 +1,15 @@
+public class UberShop {
+    public void printPrices(float[] prices) {
+        for (int i = 0; i < prices.length; i++) {
+            System.out.println(prices[i] + " jup.");
+        }
+    }
+
+    public static void main(String[] args) {
+        UberShop shop = new UberShop();
+
+        //Should be 100 jup. 23.5 jup. 400 jup. - one price per line
+        float[] prices = new float[] {100f, 23.5f, 400f};
+        shop.printPrices(prices);
+    }
+}
