6d68f5314a3081a0f0b0351c0191eb4469b5e94e
Vadim Korolov <vadim.v.korolov@gmail.com>
2025-10-26 00:10:55 +0300

    Task completed 11

--- src/main/java/ArlanHelper.java
+++ src/main/java/ArlanHelper.java
@@ -17,11 +17,33 @@ public class ArlanHelper {
         return starLine;
     }
 
+    public String drawRect(int width, int height, char c) {
+        String charLine = "";
+
+        while (height > 0) {
+            int w = width;
+            while (w > 0) {
+                charLine += c;
+                w--;
+            }
+            charLine += "\n";
+            height--;
+        }
+        return charLine;
+    }
+
     public static void main(String[] args) {
         ArlanHelper helper = new ArlanHelper();
+
         //Should be:
         //**
         //**
         System.out.println(helper.drawQuad(2));
+
+        //Should be:
+        //XX
+        //XX
+        //XX
+        System.out.println(helper.drawRect(2, 3, 'X'));
     }
 }
